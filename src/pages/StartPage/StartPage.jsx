5c484353a4613e748c550639c935f2e91c8634a3
anrikopaliani <anrikopa@gmail.com>
2023-06-05 23:08:16 +0400

    refactor:create svg component

--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -1,4 +1,4 @@
-import logo from '../../assets/Vector.svg';
+import VectorIcon from 'src/icons/VectorIcon';
 import classes from './StartPage.module.css';
 
 const StartPage = () => {
@@ -6,7 +6,7 @@ const StartPage = () => {
     <div
       className={`h-screen flex items-center justify-center flex-col  ${classes.container}`}
     >
-      <img src={logo} alt='' className='absolute top-343' />
+      <VectorIcon />
       <div className=' overflow-hidden'>
         <h1
           className={`text-center font-extrabold text-3xl overflow-hidden opacity-0 hover:cursor-pointer text-black ${classes.heading}`}
