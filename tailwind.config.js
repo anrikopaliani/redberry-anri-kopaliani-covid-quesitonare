c17950132d71c232b8cf4857ed7a2539f3644df1
anrikopaliani <anrikopa@gmail.com>
2023-06-06 02:41:13 +0400

    feat: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,23 @@ export default {
       spacing: {
         343: '343px',
         1.8: '1.8px',
+        513: '513px',
+        50: '50px',
+        0.8: '0.8px',
+        47: '37px',
       },
       colors: {
         black: '#232323',
+        error: '#F15524',
+      },
+      borderWidth: {
+        DEFAULT: '1px',
+        0.8: '0.8px',
+        2: '2px',
+        3: '3px',
+        4: '4px',
+        6: '6px',
+        8: '8px',
       },
     },
   },
