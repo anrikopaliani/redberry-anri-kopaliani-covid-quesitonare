6388db742f297368f51c0da23c6598b14ff34e9f
anrikopaliani <anrikopa@gmail.com>
2023-06-08 14:11:29 +0400

    refactor: conver px in rem in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ export default {
   theme: {
     extend: {
       spacing: {
-        343: '343px',
-        1.8: '1.8px',
-        513: '513px',
-        50: '50px',
-        0.8: '0.8px',
-        47: '37px',
+        343: '21.438rem',
+        1.8: '0.113rem',
+        513: '32.063rem',
+        50: '3.125rem',
+        0.8: '0.05rem',
+        47: '2.938rem',
       },
       colors: {
         black: '#232323',
@@ -17,13 +17,7 @@ export default {
         hint: '#626262',
       },
       borderWidth: {
-        DEFAULT: '1px',
-        0.8: '0.8px',
-        2: '2px',
-        3: '3px',
-        4: '4px',
-        6: '6px',
-        8: '8px',
+        0.8: '0.05rem',
       },
     },
   },
