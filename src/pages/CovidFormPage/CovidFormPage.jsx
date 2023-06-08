e7e1797203990674796d61c616d91cbf1cbcddc6
anrikopaliani <anrikopa@gmail.com>
2023-06-08 16:04:06 +0400

    style:create form page about covid

---
+++ src/pages/CovidFormPage/CovidFormPage.jsx
@@ -0,0 +1,11 @@
+import { Wrapper, Header } from '@/components';
+
+const CovidFormPage = () => {
+  return (
+    <Wrapper>
+      <Header pageNumber={2} />
+    </Wrapper>
+  );
+};
+
+export default CovidFormPage;
