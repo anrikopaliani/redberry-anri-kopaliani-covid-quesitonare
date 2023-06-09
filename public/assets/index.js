973489d1d2a7425e888110facd4fdb1a501a1d49
anrikopaliani <anrikopa@gmail.com>
2023-06-10 00:03:59 +0400

    refactor: add index.js file in assets folder

---
+++ public/assets/index.js
@@ -0,0 +1,2 @@
+export { default as CredentialsPicture } from './credentials-picture.png';
+export { default as SickPerson } from './sick-person.png';
--- public/index.js
+++ public/index.js
@@ -1,2 +1,1 @@
-export { default as CredentialsPicture } from './assets/credentials-picture.png';
-export { default as SickPerson } from './assets/sick-person.png';
+export * from './assets';
