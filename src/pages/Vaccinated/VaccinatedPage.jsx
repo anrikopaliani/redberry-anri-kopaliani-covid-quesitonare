2413669e887a352adaf36b3152b408ea2223702d
anrikopaliani <anrikopa@gmail.com>
2023-06-11 02:27:00 +0400

    feat: add a picture

--- src/pages/Vaccinated/VaccinatedPage.jsx
+++ src/pages/Vaccinated/VaccinatedPage.jsx
@@ -1,11 +1,15 @@
 import { Wrapper, Header } from '@/components';
 import { VaccinatedForm } from '@/components/Forms';
+import { DoctorPicture } from 'public';
 
 const VaccinatedPage = () => {
   return (
     <Wrapper>
       <Header pageNumber={3} />
-      <VaccinatedForm />
+      <div className='flex justify-between'>
+        <VaccinatedForm />
+        <img src={DoctorPicture} alt='sick person' />
+      </div>
     </Wrapper>
   );
 };
