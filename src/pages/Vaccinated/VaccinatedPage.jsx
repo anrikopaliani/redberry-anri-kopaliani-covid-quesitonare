493ba15a6256bffda902ab95bcc81a48f4d78a00
anrikopaliani <anrikopa@gmail.com>
2023-06-11 01:10:56 +0400

    feat:create vaccinted form page

---
+++ src/pages/Vaccinated/VaccinatedPage.jsx
@@ -0,0 +1,11 @@
+import { Wrapper, Header } from '@/components';
+
+const VaccinatedPage = () => {
+  return (
+    <Wrapper>
+      <Header pageNumber={3} />
+    </Wrapper>
+  );
+};
+
+export default VaccinatedPage;
